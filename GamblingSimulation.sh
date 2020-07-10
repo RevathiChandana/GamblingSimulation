c41f1c7e5641360031f080010f2fe87ad44bfa82
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-10 18:54:48 +0530

    [REVATHI]ADD UC3: gambler if win or lost 50% of the stake,would resign for the day

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -2,16 +2,32 @@
 echo "===========Gambling simulation Problem==========="
 startAmount=100;
 betAmount=1;
+totalAmount=100;
+
 function gameCheck(){
 	if [ $(($RANDOM%2)) -eq 1 ]
 	then
 		echo "won the game."
-		startAmount=$(($startAmount+$betAmount));
-		echo "Total amount : $startAmount "
+		totalAmount=$(($totalAmount+$betAmount));
+		echo "Total amount : $totalAmount "
 	else
 		echo "lose the game."
-		startAmount=$(($startAmount-$betAmount));
-		echo "Total amount : $startAmount "
+		totalAmount=$(($totalAmount-$betAmount));
+		echo "Total amount : $totalAmount "
 	fi
 }
-gameCheck
+
+count=1;
+function gameUnderCondition(){
+	while [ $count -gt 0 ]
+	do
+		gameCheck;
+		if [[ $totalAmount -eq $(($startAmount/2)) || $totalAmount -eq $(($startAmount+$((startAmount/2)))) ]]
+		then
+			echo "total amount : $totalAmount"
+			exit;
+		fi
+		((count++))
+	done
+}
+gameUnderCondition
