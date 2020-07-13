601731cdc5f89bd8dc430371acec687a34411f97
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-13 20:38:49 +0530

    [REVATHI]refactor: changes done on constant variables

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,50 +1,45 @@
 #! /bin/bash
 echo "===========Gambling simulation Problem==========="
-startAmount=100;
-betAmount=1;
+START_AMOUNT=100;
+BET_AMOUNT=1;
 declare -a winningDays;
 declare -a lossingDays;
 function gameCheck(){
 	if [ $(($RANDOM%2)) -eq 1 ]
 	then
-		#echo "won the game."
-		totalAmount=$(($totalAmount+$betAmount));
-		#echo "Total amount : $totalAmount "
+		totalAmount=$(($totalAmount+$BET_AMOUNT));
 	else
-		#echo "lose the game."
-		totalAmount=$(($totalAmount-$betAmount));
-		#echo "Total amount : $totalAmount "
+		totalAmount=$(($totalAmount-$BET_AMOUNT));
 	fi
 }
 
-count=1;
-totalAmount=100;
+COUNT=1;
 function gameUnderCondition(){
-for (( day=1; day<=20; day++ ))
-do
-	echo "day $day the person start with $startAmount"
-	while [ $count -gt 0 ]
+	totalAmount=$START_AMOUNT;
+	for (( day=1; day<=20; day++ ))
 	do
-		gameCheck;
-		if [[ $totalAmount -eq 0 ]]
-		then
-         echo "the Person lost all the money and he quit the game"
-			exit;
-		elif [[ $totalAmount -eq $(($startAmount/2)) ]]
-		then
-			echo "day $day	total amount : $totalAmount"
-			lossingDays[$day]=$day;
-			break;
-		elif [[ $totalAmount -eq $(($startAmount+$(($startAmount/2)))) ]]
-		then
-			echo "day $day	total amount : $totalAmount"
-			winningDays[$day]=$day;
-			break;
-		fi
-		((count++))
+		while [ $COUNT -gt 0 ]
+		do
+			gameCheck;
+			if [[ $totalAmount -eq 0 ]]
+			then
+         	echo "the Person lost all the money and he quit the game"
+				exit;
+			elif [[ $totalAmount -eq $(($START_AMOUNT/2)) ]]
+			then
+				echo "starts with :$START_AMOUNT - day $day - total amount : $totalAmount"
+				lossingDays[$day]=$day;
+				break;
+			elif [[ $totalAmount -eq $(($START_AMOUNT+$(($START_AMOUNT/2)))) ]]
+			then
+				echo "starts with :$START_AMOUNT - day $day - total amount : $totalAmount"
+				winningDays[$day]=$day;
+				break;
+			fi
+			((COUNT++))
+		done
+		START_AMOUNT=$totalAmount;
 	done
-	startAmount=$totalAmount;
-done
 }
 gameUnderCondition
 echo "list of winning days ${winningDays[@]}"
