7112d5a1a48bd897ecf4dd1d2114c76f310b1a41
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-13 20:18:01 +0530

    [REVATHI]refactor: changes done on constant variables

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,28 +1,28 @@
 #! /bin/bash
 echo "===========Gambling simulation Problem==========="
-startAmount=100;
-betAmount=1;
-totalAmount=100;
+START_AMOUNT=100;
+BET_AMOUNT=1;
 
 function gameCheck(){
 	if [ $(($RANDOM%2)) -eq 1 ]
 	then
 		echo "won the game."
-		totalAmount=$(($totalAmount+$betAmount));
+		totalAmount=$(($totalAmount+$BET_AMOUNT));
 		echo "Total amount : $totalAmount "
 	else
 		echo "lose the game."
-		totalAmount=$(($totalAmount-$betAmount));
+		totalAmount=$(($totalAmount-$BET_AMOUNT));
 		echo "Total amount : $totalAmount "
 	fi
 }
 
 count=1;
 function gameUnderCondition(){
+	total_Amount=$START_AMOUNT;
 	while [ $count -gt 0 ]
 	do
 		gameCheck;
-		if [[ $totalAmount -eq $(($startAmount/2)) || $totalAmount -eq $(($startAmount+$((startAmount/2)))) ]]
+		if [[ $totalAmount -eq $(($START_AMOUNT/2)) || $totalAmount -eq $(($START_AMOUNT+$(($START_AMOUNT/2)))) ]]
 		then
 			echo "total amount : $totalAmount"
 			exit;
