513b31f529be684c339bd27e35a875aa631b7629
Revathi Chandana <chandanarevathi30@gmail.com>
2020-07-13 20:49:10 +0530

    [REVATHI]refactor: changes done on constant variables

--- GamblingSimulation.sh
+++ GamblingSimulation.sh
@@ -1,67 +1,63 @@
 #! /bin/bash
 echo "===========Gambling simulation Problem==========="
-startAmount=100;
-betAmount=1;
+START_AMOUNT=100;
+BET_AMOUNT=1;
 declare -a winningDays;
 declare -a lossingDays;
 function gameCheck(){
 	if [ $(($RANDOM%2)) -eq 1 ]
 	then
-		#echo "won the game."
-		totalAmount=$(($totalAmount+$betAmount));
-		#echo "Total amount : $totalAmount "
+		totalAmount=$(($totalAmount+$BET_AMOUNT));
 	else
-		#echo "lose the game."
-		totalAmount=$(($totalAmount-$betAmount));
-		#echo "Total amount : $totalAmount "
+		totalAmount=$(($totalAmount-$BET_AMOUNT));
 	fi
 }
 
-count=1;
-totalAmount=100;
-luckiestDay=0;
-unLuckiestDay=0;
-highAmount=$startAmount;
-lowAmount=$startAmount;
+COUNT=1;
+totalAmount=$START_AMOUNT;
+LUCKY_DAY=0;
+UNLUCKY_DAY=0;
+highAmount=$START_AMOUNT;
+lowAmount=$START_AMOUNT;
+NUM_OF_DAYS=20;
 function gameUnderCondition(){
-for (( day=1; day<=20; day++ ))
-do
-	echo "day $day the person start with $startAmount"
-	while [ $count -gt 0 ]
+	for (( day=1; day<=NUM_OF_DAYS; day++ ))
 	do
-		gameCheck;
-		if [[ $totalAmount -eq 0 ]]
+		while [ $COUNT -gt 0 ]
+		do
+			gameCheck;
+			if [[ $totalAmount -eq 0 ]]
+			then
+         	echo "the Person lost all the money and he quit the game"
+				exit;
+			elif [[ $totalAmount -eq $(($START_AMOUNT/2)) ]]
+			then
+				echo "starts with :$START_AMOUNT - day $day - total amount : $totalAmount"
+				lossingDays[$day]=$day;
+				break;
+			elif [[ $totalAmount -eq $(($START_AMOUNT+$(($START_AMOUNT/2)))) ]]
+			then
+				echo "starts with :$START_AMOUNT - day $day - total amount : $totalAmount"
+				winningDays[$day]=$day;
+				break;
+			fi
+			((count++))
+		done
+		if [ $highAmount -lt $totalAmount ]
 		then
-         echo "the Person lost all the money and he quit the game"
-			exit;
-		elif [[ $totalAmount -eq $(($startAmount/2)) ]]
+			LUCKY_DAY=$day;
+			highAmount=$totalAmount;
+		elif [ $lowAmount -gt $totalAmount ]
 		then
-			echo "day $day	total amount : $totalAmount"
-			lossingDays[$day]=$day;
-			break;
-		elif [[ $totalAmount -eq $(($startAmount+$(($startAmount/2)))) ]]
-		then
-			echo "day $day	total amount : $totalAmount"
-			winningDays[$day]=$day;
-			break;
+			lowAmount=$totalAmount;
+			UNLUCKY_DAY=$day;
 		fi
-		((count++))
+		START_AMOUNT=$totalAmount;
 	done
-	if [ $highAmount -lt $totalAmount ]
-	then
-		luckiestDay=$day;
-		highAmount=$totalAmount;
-	elif [ $lowAmount -gt $totalAmount ]
-	then
-		lowAmount=$totalAmount;
-		unLuckiestDay=$day;
-	fi
-	startAmount=$totalAmount;
-done
 }
 gameUnderCondition
 echo "list of winning days ${winningDays[@]}"
 echo "list of loosing days ${lossingDays[@]}"
-echo "luckiestday : $luckiestDay and amount : $highAmount"
-echo "unLuckiestDay : $unLuckiestDay and amount :$lowAmount"
+echo "luckiestday : $LUCKY_DAY and amount : $highAmount"
+echo "unLuckiestDay : $UNLUCKY_DAY and amount :$lowAmount"
 
